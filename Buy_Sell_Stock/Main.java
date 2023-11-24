8e9e831a9dc1eaa57ef1ffcf974e5717ba52ea80
Yenleng <yenlengggggg@gmail.com>
2023-11-25 01:34:02 +0800

    add explanations

--- Buy_Sell_Stock/Main.java
+++ Buy_Sell_Stock/Main.java
@@ -4,9 +4,14 @@ class Solution {
         int sellRight = 1;
         int profit = 0;
         for(int i = 0 ; i < prices.length-1; i ++) {
+
+            // when buy price is more than sell price, shift buy to sell to get a lower buy number, shift sell to the right
             if(prices[sellRight] < prices[buyLeft]) {
                 buyLeft = sellRight;
                 sellRight++;
+            
+            // if sell price is more than buy, check if current profit is more than existing profit. if current > more, update profit numbers
+            // shift sell price to right again to compare with buy price
             } else if (prices[buyLeft] < prices[sellRight]) {
                 
                 int currentProfit = prices[sellRight] - prices[buyLeft];
@@ -14,7 +19,8 @@ class Solution {
                     profit = currentProfit;
                 }
                 sellRight++;
-            } else if (prices[sellRight] == prices[buyLeft] || prices[sellRight] == 0) {
+            // if buy price = sell, there is no profit so move sell price to the right to compare again
+            } else if (prices[sellRight] == prices[buyLeft] ) {
                 sellRight ++;
             }
             
