b009926f24a76cc460acea82245dd0e1abc31541
Yenleng <yenlengggggg@gmail.com>
2023-12-05 19:55:11 +0800

    add readme

--- Max_Depth_Tree/TreeNode.java
+++ Max_Depth_Tree/TreeNode.java
@@ -1,7 +1,26 @@
-class Solution {
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
     public int maxDepth(TreeNode root) {
         // set base code for recursion, 0 means the end of the tree
-        if (root == null) return 0;
+        if (root == null)
+            return 0;
 
         // to move down left branch and right branch
         int left = maxDepth(root.left);
@@ -9,7 +28,7 @@ class Solution {
 
         // get the highest number between left & right
         // +1 to include the head of the tree
-        return 1 + Math.max(left,right);
+        return 1 + Math.max(left, right);
 
     }
 }
